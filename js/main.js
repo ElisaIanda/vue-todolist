db8d486a252581c66a1cb1e2f0afb679fe32d533
ElisaIanda <elisaianda01@gmail.com>
2023-07-21 15:18:25 +0200

    Creato input testo per aggiungere nuovo task

--- js/main.js
+++ js/main.js
@@ -2,11 +2,49 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            listTask: [
+                {
+                    id: "01",
+                    text: "Brainstorming",
+                    done: "true",
+                },
+                {
+                    id: "02",
+                    text: "Creazione grafiche",
+                    done: "false",
+                },
+                {
+                    id: "03",
+                    text: "Architettura sito web",
+                    done: "false",
+                },
+                {
+                    id: "04",
+                    text: "Analizzare dati",
+                    done: "true",
+                },
+                {
+                    id: "05",
+                    text: "Inserire immagini",
+                    done: "true",
+                }
+            ],
+
+            newTask: {
+                id: "",
+                text: "",
+                done: "",
+            },
         };
     },
     methods: {
+        addNewTask() {
+            // Clono l'arrey per il problema reattività
+            const newTaskClone = {...this.newTask}
 
+            this.listTask.push(newTaskClone)
+            console.log(newTaskClone)
+        }
     },
 });
 
